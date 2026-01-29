84933f8111cde70da0ea337cf2e27d700b610fd4
krosswelcauro <krosswelcauro@gmail.com>
2026-01-29 03:23:55 -0400

    feat: funcionalidad listar propiedades

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,20 @@
 <?php 
 
+    // Importar la conexion
+    require '../includes/config/database.php';
+    $db = conectarDB();
+    
+    // Escribir el query
+    $query = "SELECT * FROM propiedades";
+
+    // Consultar la BD
+    $resultado_query = mysqli_query($db, $query);
+
+
+    // Muestra un mensaje condicional
     $resultado = $_GET['resultado'] ?? null;
 
+    // Incluye un template
     require '../includes/funciones.php';
     incluirTemplate('header');
 ?>
@@ -13,8 +26,38 @@
         <?php endif; ?>
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody> <!-- Mostrar los resultados -->
+                <?php while( $propiedad = mysqli_fetch_assoc($resultado_query)) : ?>
+                <tr>
+                    <td><?php echo $propiedad['id']; ?></td>
+                    <td><?php echo $propiedad['titulo']; ?></td>
+                    <td> <img src="/imagenes/<?php echo $propiedad['imagen']; ?>" class="imagen-tabla"> </td>
+                    <td>$ <?php echo $propiedad['precio']; ?></td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
+
     </main>
 
 <?php 
+    // Cerrar la conexion
+    mysqli_close($db);
+    
     incluirTemplate('footer');
 ?>
--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -25,9 +25,9 @@
         //     var_dump($_POST);
         // echo "</pre>";
 
-        echo "<pre>";
-            var_dump($_FILES);
-        echo "</pre>";
+        // echo "<pre>";
+        //     var_dump($_FILES);
+        // echo "</pre>";
 
         $titulo = mysqli_real_escape_string( $db, $_POST['titulo'] );
         $precio = mysqli_real_escape_string( $db, $_POST['precio'] );
